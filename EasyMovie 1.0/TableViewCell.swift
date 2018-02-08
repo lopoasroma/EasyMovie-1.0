8c2b1959752be1539c8864efae1cc8ee29a9466b
Anna de Rosa <annaderosa@cam-08.local>
2018-02-08 11:49:54 +0100

    aggiunta delle tableviewcell e tableviewController

---
+++ EasyMovie 1.0/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  EasyMovie 1.0
+//
+//  Created by Anna de Rosa on 08/02/18.
+//  Copyright © 2018 Luigi Procentese. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
