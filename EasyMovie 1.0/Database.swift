863fea2a9dcd357b1f2f2bc14c0b9c0f5b2d34bd
Giuseppe Graniero <giuseppegraniero@10-26-48-43.0200.cam.it.iosda.org>
2018-02-08 12:20:43 +0100

    Aggiunti assets nel database

--- EasyMovie 1.0/Database.swift
+++ EasyMovie 1.0/Database.swift
@@ -38,19 +38,19 @@ struct videoPlay {
     var salvato: Bool
 }
 
-var consigliU = categoriaHome(nome: "Consigli utili", difficoltà: .Basso, immagine: <#T##UIImage#>, descrizione: "Qui troverai consigli utili per tutti i programmi di video editing. Consulta queste pagine prima di iniziare!")
+var consigliU = categoriaHome(nome: "Consigli utili", difficoltà: .Basso, immagine: #imageLiteral(resourceName: "Consigli"), descrizione: "Qui troverai consigli utili per tutti i programmi di video editing. Consulta queste pagine prima di iniziare!")
 
-var movieMaker = categoriaHome(nome: "Windows Movie Maker", difficoltà: .Basso, immagine: <#T##UIImage#>, descrizione: "")
+var movieMaker = categoriaHome(nome: "Windows Movie Maker", difficoltà: .Basso, immagine: #imageLiteral(resourceName: "Windows-movie-maker-2012-08-535x5351"), descrizione: "")
 
-var pinnacleStudio = categoriaHome(nome: "Pinnacle Studio", difficoltà: .Medio, immagine: <#T##UIImage#>, descrizione: "")
+var pinnacleStudio = categoriaHome(nome: "Pinnacle Studio", difficoltà: .Medio, immagine: #imageLiteral(resourceName: "rounded-icon"), descrizione: "")
 
-var iMovie = categoriaHome(nome: "iMovie", difficoltà: .MedioAlto, immagine: <#T##UIImage#>, descrizione: "")
+var iMovie = categoriaHome(nome: "iMovie", difficoltà: .MedioAlto, immagine: #imageLiteral(resourceName: "imovie-mac-icon-100692968-orig"), descrizione: "")
 
-var wondershareFilmora = categoriaHome(nome: "Wondershare Filmora", difficoltà: .MedioAlto, immagine: <#T##UIImage#>, descrizione: "")
+var wondershareFilmora = categoriaHome(nome: "Wondershare Filmora", difficoltà: .MedioAlto, immagine: #imageLiteral(resourceName: "filmora"), descrizione: "")
 
-var sonyVegas = categoriaHome(nome: "Sony Vegas", difficoltà: .Alto, immagine: <#T##UIImage#>, descrizione: "")
+var sonyVegas = categoriaHome(nome: "Sony Vegas", difficoltà: .Alto, immagine: #imageLiteral(resourceName: "sony-vegas"), descrizione: "")
 
-var hitFilm = categoriaHome(nome: "Hitfilm", difficoltà: .Alto, immagine: <#T##UIImage#>, descrizione: "")
+var hitFilm = categoriaHome(nome: "Hitfilm", difficoltà: .Alto, immagine: #imageLiteral(resourceName: "IMG_0460"), descrizione: "")
 
 var elencoCategorie = [consigliU,movieMaker,iMovie,pinnacleStudio,wondershareFilmora,sonyVegas,hitFilm]
 
