16b5d1feeab4c01ee769b1eb31d3849307f8a8fb
Luigi Procentese <luigiprocentese@cam-22.local>
2018-02-08 09:36:50 +0100

    Initial Commit

---
+++ EasyMovie 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyMovie 1.0
+//
+//  Created by Luigi Procentese on 08/02/18.
+//  Copyright © 2018 Luigi Procentese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
