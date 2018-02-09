86e543de5049a950a870764e1e02101ef6e66ddf
Giuseppe Graniero <giuseppegraniero@mac-di-giuseppe.local>
2018-02-09 12:23:02 +0100

    funziona fino alla seconda schermata

--- EasyMovie 1.0/TableViewControllerConsigliUtii.swift
+++ EasyMovie 1.0/TableViewControllerConsigliUtii.swift
@@ -14,6 +14,8 @@ class TableViewControllerConsigliUtii: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.title = elementoSelezionato.nome
+
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
