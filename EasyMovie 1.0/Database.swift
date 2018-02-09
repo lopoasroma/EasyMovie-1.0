807a642b283da4eae4beb7934d4ce3ddf5a5e8ba
Giuseppe Graniero <giuseppegraniero@mac-di-giuseppe.local>
2018-02-09 10:43:15 +0100

    Aggiornamento assets e database

--- EasyMovie 1.0/Database.swift
+++ EasyMovie 1.0/Database.swift
@@ -50,28 +50,28 @@ var elencoCategorie = [intro,consigliU,movieMaker,iMovie,pinnacleStudio,wondersh
 
 
 
-var consigli1 = videoLista(categoria: consigliU, titolo: "Come ruotare un video su Mac", data: "25 Giu 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var consigli2 = videoLista(categoria: consigliU, titolo: "Esportare un filmato senza perdita di qualità", data: "4 Ago 2014", autore: "Fabrizio Gherardi", immagine: UIImage)
-var consigli3 = videoLista(categoria: consigliU, titolo: "5 consigli per velocizzare il video editing", data: "18 Nov 2017", autore: "PaoloG Youtube", immagine: <#T##UIImage#>)
-var consigli4 = videoLista(categoria: consigliU, titolo: "Qual è il miglior formato per YouTube", data: "26 Dic 2017", autore: "PaoloG Youtube", immagine: <#T##UIImage#>)
-var consigli5 = videoLista(categoria: consigliU, titolo: "Come migliorare l'audio nei tuoi video", data: "4 Dic 2014", autore: "PaoloG Youtube", immagine: <#T##UIImage#>)
-var consigli6 = videoLista(categoria: consigliU, titolo: "Migliorare l'illuminazione nei tuoi video", data: "12 Dic 2014", autore: "PaoloG Youtube", immagine: <#T##UIImage#>)
-var consigli7 = videoLista(categoria: consigliU, titolo: "Come avere uno sfondo neutro", data: "16 Gen 2015", autore: "PaoloG Youtube", immagine: <#T##UIImage#>)
-var consigli8 = videoLista(categoria: consigliU, titolo: "Avere lo sfondo totalmente bianco stile Apple", data: "7 Mar 2015", autore: "PaoloG Youtube", immagine: <#T##UIImage#>)
+var consigli1 = videoLista(titolo: "Come ruotare un video su Mac", data: "25 Giu 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "RuotareMac"))
+var consigli2 = videoLista(titolo: "Esportare un filmato senza perdita di qualità", data: "4 Ago 2014", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "Esportare"))
+var consigli3 = videoLista(titolo: "5 consigli per velocizzare il video editing", data: "18 Nov 2017", autore: "PaoloG Youtube", immagine: #imageLiteral(resourceName: "5Consigli"))
+var consigli4 = videoLista(titolo: "Qual è il miglior formato per YouTube", data: "26 Dic 2017", autore: "PaoloG Youtube", immagine: #imageLiteral(resourceName: "Miglior Formato"))
+var consigli5 = videoLista(titolo: "Come migliorare l'audio nei tuoi video", data: "4 Dic 2014", autore: "PaoloG Youtube", immagine: #imageLiteral(resourceName: "QualitàAudio"))
+var consigli6 = videoLista(titolo: "Migliorare l'illuminazione nei tuoi video", data: "12 Dic 2014", autore: "PaoloG Youtube", immagine: #imageLiteral(resourceName: "Illuminazione"))
+var consigli7 = videoLista(titolo: "Come avere uno sfondo neutro", data: "16 Gen 2015", autore: "PaoloG Youtube", immagine: #imageLiteral(resourceName: "SfondoNeutro"))
+var consigli8 = videoLista(titolo: "Avere lo sfondo totalmente bianco stile Apple", data: "7 Mar 2015", autore: "PaoloG Youtube", immagine: #imageLiteral(resourceName: "SfondoApple"))
 
-var movieMaker1 = videoLista(categoria: movieMaker, titolo: "Corso completo di Movie Maker-Parte 1", data: "26 Luglio 2016", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var movieMaker2 = videoLista(categoria: movieMaker, titolo: "Corso completo di Movie Maker-Parte 2", data: "26 Luglio 2016", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var movieMaker3 = videoLista(categoria: movieMaker, titolo: "Corso completo di Movie Maker-Parte 3", data: "26 Luglio 2016", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
+var movieMaker1 = videoLista(titolo: "Corso completo di Movie Maker-Parte 1", data: "26 Luglio 2016", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "MovieM1"))
+var movieMaker2 = videoLista(titolo: "Corso completo di Movie Maker-Parte 2", data: "26 Luglio 2016", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "MovieM2"))
+var movieMaker3 = videoLista(titolo: "Corso completo di Movie Maker-Parte 3", data: "26 Luglio 2016", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "MovieM3"))
 
 
-var imovie = videoLista(categoria: iMovie , titolo: "Layout e inserimento video", data: "9 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie1 = videoLista(categoria: iMovie , titolo: "Le transizioni,le mappe e gli sfondi", data: "11 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie2 = videoLista(categoria: iMovie , titolo: "Settaggi video e titoli", data: "12 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie3 = videoLista(categoria: iMovie , titolo: "Effetti audio e suoni", data: "14 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie4 = videoLista(categoria: iMovie , titolo: "Gli effetti video", data: "16 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie5 = videoLista(categoria: iMovie , titolo: "I temi", data: "18 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie6 = videoLista(categoria: iMovie , titolo: "I trailer", data: "18 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
-var imovie7 = videoLista(categoria: iMovie , titolo: "Il rendering", data: "21 Mar 2017", autore: "Fabrizio Gherardi", immagine: <#T##UIImage#>)
+var imovie = videoLista(titolo: "Layout e inserimento video", data: "9 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie1"))
+var imovie1 = videoLista(titolo: "Le transizioni,le mappe e gli sfondi", data: "11 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie2"))
+var imovie2 = videoLista(titolo: "Settaggi video e titoli", data: "12 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie3"))
+var imovie3 = videoLista(titolo: "Effetti audio e suoni", data: "14 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie 4"))
+var imovie4 = videoLista(titolo: "Gli effetti video", data: "16 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie5"))
+var imovie5 = videoLista(titolo: "I temi", data: "18 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie6"))
+var imovie6 = videoLista(titolo: "I trailer", data: "18 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie7"))
+var imovie7 = videoLista(titolo: "Il rendering", data: "21 Mar 2017", autore: "Fabrizio Gherardi", immagine: #imageLiteral(resourceName: "iMovie8"))
 
 
 
